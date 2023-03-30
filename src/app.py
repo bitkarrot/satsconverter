3b2e9b1ad04f0a2835a9146ff7cd9a14ec809faf
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2023-03-29 22:15:59 -0700

    add styles

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html", title="sats converter")
+    return render_template("index.html", title="Sats Converter")
 #    return 'Hello, World!'
 
 @app.route('/about')
